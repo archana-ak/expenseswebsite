678749cbdbd2d1ae6d940a093c9e2f50e747b354
archana.kumari@tecvololabs.com <archanakumari472.ak@gmail.com>
2022-06-15 12:57:49 +0530

    Initial commit

---
+++ expenseswebsite/env.bat
@@ -0,0 +1,8 @@
+set DB_NAME=incomeexpensesdb
+set DB_USER=postgres
+set DB_USER_PASSWORD=12345
+set DB_HOST=localhost
+set EMAIL_HOST_PASSWORD=archana@9521
+set EMAIL_HOST_USER=programmers.project.ak@gmail.com
+DEFAULT_FROM_EMAIL = programmers.project.ak@gmail.com
+set EMAIL_HOST=smtp.gmail.com
